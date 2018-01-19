3644f372892cfd540ff95e28b464136826568142
Sean Cheung <theoxuanx@gmail.com>
2018-01-20 03:31:08 +0800

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+MYSQL_INITSQL=/.init
+
+function boot_mysql()
+{   
+    mkdir -p /var/run/mysqld
+    chown -R mysql:mysql /var/run/mysqld
+
+    bootfile=$1
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '';" > $bootfile
+
+    if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
+        echo "[Mysql] updating root password"
+        echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;" >> $bootfile
+    fi
+
+    # MYSQL_USER: "username:password" or "username". password will be the same as username if omitted.
+    # For multiple user creation, seperate them with ";".
+    if [ -n "$MYSQL_USER" ]; then
+        IFS=';'; users=($MYSQL_USER); unset IFS;
+        for entry in "${users[@]}"; do
+            IFS=':'; sub=($entry); unset IFS;
+            if [ ${#sub[@]} -eq 1 ]; then
+                username=${sub[0]}
+                password=$username
+            elif [ ${#sub[@]} -eq 2 ]; then
+                username=${sub[0]}
+                password=${sub[1]}
+            else
+                echo "[Mysql] invalid username in ${MYSQL_USER}"
+                exit 1
+            fi
+            echo "[Mysql] create user ${username}"
+            echo "CREATE USER IF NOT EXISTS '${username}'@'%' IDENTIFIED BY '${password}';" >> $bootfile
+        done
+    fi
+
+    # MYSQL_DATABASE: "username@database" or "database". username will be the same as database if omitted.
+    # User will be created(with password the same as username) if not exist.
+    # For multiple database creation, seperate them with ";".
+    if [ -n "$MYSQL_DATABASE" ]; then
+        IFS=';'; ary=($MYSQL_DATABASE); unset IFS;
+        for entry in "${ary[@]}"; do
+            IFS='@'; sub=($entry); unset IFS;
+            if [ ${#sub[@]} -eq 1 ]; then
+                username=${sub[0]}
+                database=$username
+            elif [ ${#sub[@]} -eq 2 ]; then
+                username=${sub[0]}
+                database=${sub[1]}
+            else
+                echo "[Mysql] invalid database in ${MYSQL_DATABASE}"
+                exit 1
+            fi
+            echo "[Mysql] create database ${database}"
+            echo "CREATE DATABASE IF NOT EXISTS \`${database}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $bootfile
+            # ensure user exists
+            echo "CREATE USER IF NOT EXISTS '${username}'@'%' IDENTIFIED BY '${username}';" >> $bootfile
+            echo "[Mysql] grant privileges to ${username} on ${database}"
+            echo "GRANT ALL PRIVILEGES ON \`${database}\`.* to '${username}'@'%';" >> $bootfile
+        done
+    fi
+
+    echo "FLUSH PRIVILEGES;" >> $bootfile
+}
+
+if [ ! -f "$MYSQL_INITSQL" ] && [ -z "$MYSQL_SKIP_INIT" ]; then
+    boot_mysql "$MYSQL_INITSQL"
+fi
+
+exec "$@"
+
